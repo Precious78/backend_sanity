3e64b47cc9f68c418da74a3adbcd601ec6b52ff0
Precious78 <ebohprecious724@gmail.comm>
2022-07-15 10:17:30 +0100

    First Project on sanity

---
+++ schemas/abouts.js
@@ -0,0 +1,30 @@
+export default {
+    name: 'abouts',
+    title: 'Abouts',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string'
+        },
+        {
+            name: 'imageurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string'
+        },
+    ],
+}
